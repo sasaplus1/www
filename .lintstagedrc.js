50abb3ab3231e992d3efc9c38a61b9c97223f77c
sasaplus1 <sasaplus1@gmail.com>
2019-11-23 09:24:14 +0900

    add lint rules to lint-staged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,3 +1,9 @@
 module.exports = {
+  '*.js': 'npx eslint --cache',
+  'package.json': [
+    'npx fixpack',
+    'npx prettier --parser json-stringify --write',
+    'git diff --exit-code --quiet'
+  ],
   'package-lock.json': 'node -e "process.exitCode = 1;"'
 };
