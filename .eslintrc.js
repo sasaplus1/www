954a9a63e5a6913a18597c6009df7e1993ad7c11
sasaplus1 <sasaplus1@gmail.com>
2019-11-23 09:13:48 +0900

    add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:node/recommended',
+    'plugin:prettier/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2019
+  },
+  root: true
+};
