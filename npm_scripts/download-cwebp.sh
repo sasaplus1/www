c8dc2425fa4ebd7f66ee3d4eab201a5615eba7f4
sasaplus1 <sasaplus1@gmail.com>
2019-12-05 19:46:34 +0900

    add npm_scripts for webp

---
+++ npm_scripts/download-cwebp.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+set -euo pipefail
+
+__main() {
+  local root="$1"
+
+  if [ ! -d "$root" ]
+  then
+    printf -- 'Base directory path is not found.\n' >&2
+    exit 1
+  fi
+
+  if [ ! -f "${root}/package.json" ]
+  then
+    printf -- 'package.json not found in target directory.\n' >&2
+    exit 1
+  fi
+
+  local os=
+
+  os="$(uname)"
+
+  local url='https://storage.googleapis.com/downloads.webmproject.org/releases/webp'
+
+  case "$os" in
+    Darwin)
+      url="${url}/libwebp-1.0.3-mac-10.14.tar.gz"
+      ;;
+    Linux)
+      url="${url}/libwebp-1.0.3-linux-x86-64.tar.gz"
+      ;;
+    *)
+      printf -- 'Unsupported platform.\n' >&2
+      exit 1
+      ;;
+  esac
+
+  local tempdir=
+
+  tempdir="$(mktemp -d '/tmp/XXXXXXXXXX')"
+
+  local file="${tempdir}/libwebp.tar.gz"
+
+  if type curl >/dev/null 2>&1
+  then
+    curl -fsSL "$url" -o "$file"
+  elif type wget >/dev/null 2>&1
+  then
+    wget "$url" -O "$file"
+  else
+    printf -- 'This command needs curl or wget.\n' >&2
+    exit 1
+  fi
+
+  mkdir -p "${root}/node_modules/.bin"
+
+  tar xvfz "$file" -C "${root}/node_modules/.bin" --strip-components 2 '*/bin/cwebp' >/dev/null 2>&1
+
+  rm -rf "${tempdir}"
+
+  printf -- '%s\n' "Extract to ${root}/node_modules/.bin/cwebp."
+  printf -- 'Complete.\n'
+}
+
+__main "$@"
+
+unset -f __main
